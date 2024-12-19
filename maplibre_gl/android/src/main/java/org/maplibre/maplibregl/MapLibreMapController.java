15ec2407aabd59caaf17271c26889ccceeeb4439
Albert Moravec <albert.moravec@gmail.com>
2024-12-19 13:49:45 +0100

    fix: Check style for null

--- maplibre_gl/android/src/main/java/org/maplibre/maplibregl/MapLibreMapController.java
+++ maplibre_gl/android/src/main/java/org/maplibre/maplibregl/MapLibreMapController.java
@@ -2023,7 +2023,7 @@ final class MapLibreMapController
   }
 
   private void updateMyLocationEnabled() {
-    if (this.locationComponent == null && myLocationEnabled) {
+    if (this.locationComponent == null && mapLibreMap.getStyle() != null && myLocationEnabled) {
       enableLocationComponent(mapLibreMap.getStyle());
     }
 
