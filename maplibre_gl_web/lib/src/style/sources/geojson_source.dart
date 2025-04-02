357e33ab0009f688c2aa5276bd1ba97e114ab6c4
Albert Moravec <albert.moravec@gmail.com>
2025-04-02 12:49:55 +0200

    feat: Add interop for GeoJSON source diff update

--- maplibre_gl_web/lib/src/style/sources/geojson_source.dart
+++ maplibre_gl_web/lib/src/style/sources/geojson_source.dart
@@ -1,9 +1,11 @@
 import 'package:maplibre_gl_web/src/geo/geojson.dart';
 import 'package:maplibre_gl_web/src/interop/style/sources/geojson_source_interop.dart';
+import 'package:maplibre_gl_web/src/style/sources/geojson_source_diff.dart';
 import 'package:maplibre_gl_web/src/style/sources/source.dart';
 
 class GeoJsonSource extends Source<GeoJsonSourceJsImpl> {
   FeatureCollection get data => FeatureCollection.fromJsObject(jsObject.data);
+
   String? get promoteId => jsObject.promoteId;
 
   factory GeoJsonSource({
@@ -18,6 +20,9 @@ class GeoJsonSource extends Source<GeoJsonSourceJsImpl> {
 
   GeoJsonSource setData(FeatureCollection featureCollection) =>
       GeoJsonSource.fromJsObject(jsObject.setData(featureCollection.jsObject));
+
+  GeoJsonSource updateData(GeoJsonSourceDiff diff) =>
+      GeoJsonSource.fromJsObject(jsObject.updateData(diff.jsObject));
 
   /// Creates a new GeoJsonSource from a [jsObject].
   GeoJsonSource.fromJsObject(super.jsObject) : super.fromJsObject();
